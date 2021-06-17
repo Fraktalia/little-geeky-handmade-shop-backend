4b89e6bbf6a16ef2c13330d9785b69bd982ae796
Fraktalia <g.w.koscielniak@gmail.com>
2021-06-17 15:01:26 +0200

    Initial commit

---
+++ src/main/java/com/wilki/littlegeekyhandmade/LittleGeekyHandmadeApplication.java
@@ -0,0 +1,13 @@
+package com.wilki.littlegeekyhandmade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LittleGeekyHandmadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LittleGeekyHandmadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilki/littlegeekyhandmade/LittleGeekyHandmadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilki.littlegeekyhandmade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LittleGeekyHandmadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
